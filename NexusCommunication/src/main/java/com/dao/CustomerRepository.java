a542ffd50d4c89a22172a83e317ea797338412a9
narkhederohitk <rohitnarkhede97@gmail.com>
2021-02-27 11:30:36 +0530

    delete Project

--- NexusCommunication/src/main/java/com/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bean.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
-}
