49df87c7778e7de3abe799c9fdc4ba70c3726f82
narkhederohitk <rohitnarkhede97@gmail.com>
2021-03-01 18:10:43 +0530

    empty project

--- NexusCommunication/src/main/java/com/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bean.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
--- NexusCommunication/src/main/java/com/dao/RetailerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bean.Retailer;
-
-public interface RetailerRepository extends JpaRepository<Retailer, Long>{
-
-}
