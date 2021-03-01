dfc29d7fcfcec2ccc28b8064a260c98e8174bef0
Manthan Pelne <manthanpelne2016@gmail.com>
2021-03-01 18:00:06 +0530

    deleted

--- NexusCommunication/src/main/java/com/dao/ConnectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bean.Connection;
-
-public interface ConnectionRepository extends JpaRepository<Connection, Long> {
-
-}
